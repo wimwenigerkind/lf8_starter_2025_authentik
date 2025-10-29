4cce8dfdcfca01560ceb62237777efb487244473
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-10-29 12:08:52 +0100

    feat: implement ProjectController and related DTOs for project creation

---
+++ src/main/java/de/szut/lf8_starter/project/ProjectController.java
@@ -0,0 +1,29 @@
+package de.szut.lf8_starter.project;
+
+import de.szut.lf8_starter.project.dto.ProjectCreateDto;
+import de.szut.lf8_starter.project.dto.ProjectGetDto;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/projects")
+public class ProjectController implements ProjectControllerOpenAPI {
+    private final ProjectService projectService;
+    private final ProjectMapper projectMapper;
+
+    public ProjectController(ProjectService projectService, ProjectMapper projectMapper) {
+        this.projectService = projectService;
+        this.projectMapper = projectMapper;
+    }
+
+    @Override
+    @PostMapping
+    public ProjectGetDto create(@Valid @RequestBody ProjectCreateDto dto) {
+        ProjectEntity entity = projectMapper.mapCreateDtoToEntity(dto);
+        ProjectEntity savedEntity = this.projectService.create(entity);
+        return projectMapper.mapEntityToGetDto(savedEntity);
+    }
+}
---
+++ src/main/java/de/szut/lf8_starter/project/ProjectMapper.java
@@ -0,0 +1,55 @@
+package de.szut.lf8_starter.project;
+
+import de.szut.lf8_starter.project.dto.ProjectCreateDto;
+import de.szut.lf8_starter.project.dto.ProjectGetDto;
+import de.szut.lf8_starter.project.dto.ProjectUpdateDto;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class ProjectMapper {
+    public ProjectEntity mapCreateDtoToEntity(ProjectCreateDto dto) {
+        ProjectEntity entity = new ProjectEntity();
+        entity.setName(dto.getName());
+        entity.setResponsibleEmployeeId(dto.getResponsibleEmployeeId());
+        entity.setClientId(dto.getClientId());
+        entity.setClientContactName(dto.getClientContactName());
+        entity.setComment(dto.getComment());
+        entity.setStartDate(dto.getStartDate());
+        entity.setPlannedEndDate(dto.getPlannedEndDate());
+        entity.setActualEndDate(dto.getActualEndDate());
+
+        return entity;
+    }
+
+    public ProjectGetDto mapEntityToGetDto(ProjectEntity entity) {
+        ProjectGetDto dto = new ProjectGetDto();
+        dto.setId(entity.getId());
+        dto.setName(entity.getName());
+        dto.setResponsibleEmployeeId(entity.getResponsibleEmployeeId());
+        dto.setClientId(entity.getClientId());
+        dto.setClientContactName(entity.getClientContactName());
+        dto.setComment(entity.getComment());
+        dto.setStartDate(entity.getStartDate());
+        dto.setPlannedEndDate(entity.getPlannedEndDate());
+        dto.setActualEndDate(entity.getActualEndDate());
+
+        if (entity.getEmployees() != null) {
+            dto.setEmployees(entity.getEmployees().stream()
+                    .map(this::mapEmployeeEntityToDto)
+                    .collect(Collectors.toList()));
+        }
+
+        return dto;
+    }
+
+    private ProjectGetDto.ProjectEmployeeDto mapEmployeeEntityToDto(ProjectEntity.ProjectEmployee entity) {
+        ProjectGetDto.ProjectEmployeeDto dto = new ProjectGetDto.ProjectEmployeeDto();
+        dto.setEmployeeId(entity.getEmployeeId());
+        dto.setRoleId(entity.getRoleId());
+        dto.setStartDate(entity.getStartDate());
+        dto.setEndDate(entity.getEndDate());
+        return dto;
+    }
+}
