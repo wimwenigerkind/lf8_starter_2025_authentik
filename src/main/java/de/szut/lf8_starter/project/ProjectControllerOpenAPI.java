5a57f9e1fee294470c1548ff14ae829def3f8a3b
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-10-29 12:08:52 +0100

    feat: implement ProjectController and related DTOs for project creation

---
+++ src/main/java/de/szut/lf8_starter/project/ProjectControllerOpenAPI.java
@@ -0,0 +1,27 @@
+package de.szut.lf8_starter.project;
+
+import de.szut.lf8_starter.project.dto.ProjectCreateDto;
+import de.szut.lf8_starter.project.dto.ProjectGetDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+public interface ProjectControllerOpenAPI {
+    @Operation(summary = "creates a new Project")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "created Project",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ProjectGetDto.class))}),
+            @ApiResponse(responseCode = "400", description = "invalid JSON posted",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "not authorized",
+                    content = @Content)})
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    ProjectGetDto create(@Valid @RequestBody ProjectCreateDto dto);
+}
