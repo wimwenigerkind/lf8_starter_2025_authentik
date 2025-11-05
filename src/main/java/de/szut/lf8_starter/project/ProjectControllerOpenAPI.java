7b53933a345bf1034c6bb2d0e908948f6b5f2769
ftraue <ftraue@cvs.de>
2025-11-05 10:35:05 +0100

    feat: add endpoint to assign an employee to a project in ProjectControllerOpenAPI

--- src/main/java/de/szut/lf8_starter/project/ProjectControllerOpenAPI.java
+++ src/main/java/de/szut/lf8_starter/project/ProjectControllerOpenAPI.java
@@ -1,5 +1,7 @@
 package de.szut.lf8_starter.project;
 
+import de.szut.lf8_starter.employee.dto.EmployeeAddDto;
+import de.szut.lf8_starter.employee.dto.EmployeeGetDto;
 import de.szut.lf8_starter.project.dto.ProjectCreateDto;
 import de.szut.lf8_starter.project.dto.ProjectGetDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -24,4 +26,24 @@ public interface ProjectControllerOpenAPI {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     ProjectGetDto create(@Valid @RequestBody ProjectCreateDto dto);
+
+    @Operation(summary = "Adds an employee to a Project")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "added employee to Project",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ProjectGetDto.class))}),
+            @ApiResponse(responseCode = "400", description = "invalid JSON posted",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "not authorized",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Project or Employee not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "409", description = "Employee already assigned to Project",
+                    content = @Content),
+            @ApiResponse(responseCode = "422" , description = "qualification not met",
+                    content = @Content)
+            })
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    EmployeeGetDto add(EmployeeAddDto dto, @PathVariable String projectId);
 }
