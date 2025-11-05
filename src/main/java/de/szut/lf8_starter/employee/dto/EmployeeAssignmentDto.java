0d358219e28cf6bf7eb43339af45fe1f05ec46f8
ftraue <ftraue@cvs.de>
2025-11-05 10:43:18 +0100

    feat: add EmployeeAssignmentDto for employee assignment data transfer

---
+++ src/main/java/de/szut/lf8_starter/employee/dto/EmployeeAssignmentDto.java
@@ -0,0 +1,25 @@
+package de.szut.lf8_starter.employee.dto;
+
+import de.szut.lf8_starter.validation.ValidDateRange;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ValidDateRange(startDateField = "startDate", endDateField = "endDate")
+public class EmployeeAssignmentDto {
+    @NotNull(message = "employee id must not be null")
+    private Long employeeId;
+
+    @NotNull(message = "role id must not be null")
+    private Long roleId;
+
+    @NotNull(message = "start date must not be null")
+    private LocalDate startDate;
+
+    @NotNull(message = "end date must not be null")
+    private LocalDate endDate;
+}
