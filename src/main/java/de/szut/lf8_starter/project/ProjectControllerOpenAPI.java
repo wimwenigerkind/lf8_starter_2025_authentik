e316775514add6888e686acd0d317e321714e908
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-11-10 18:47:32 +0100

    feat: add getAll method to ProjectControllerOpenAPI interface for retrieving all projects

--- src/main/java/de/szut/lf8_starter/project/ProjectControllerOpenAPI.java
+++ src/main/java/de/szut/lf8_starter/project/ProjectControllerOpenAPI.java
@@ -3,6 +3,7 @@ package de.szut.lf8_starter.project;
 import de.szut.lf8_starter.project.dto.ProjectCreateDto;
 import de.szut.lf8_starter.project.dto.ProjectGetDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -10,6 +11,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 public interface ProjectControllerOpenAPI {
     @Operation(summary = "creates a new Project")
@@ -24,4 +27,14 @@ public interface ProjectControllerOpenAPI {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     ProjectGetDto create(@Valid @RequestBody ProjectCreateDto dto);
+
+    @Operation(summary = "get all projects")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "returned all projects",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = ProjectGetDto.class)))}),
+            @ApiResponse(responseCode = "401", description = "not authorized",
+                    content = @Content)})
+    @GetMapping
+    List<ProjectGetDto> getAll();
 }
