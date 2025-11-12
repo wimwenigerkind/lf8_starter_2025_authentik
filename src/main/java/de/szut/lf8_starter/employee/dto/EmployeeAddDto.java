50f9a4e500dda69e2be4132a9b33c576bc3d50a1
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-11-13 00:41:41 +0100

    feat: remove qualification field from EmployeeAddDto

--- src/main/java/de/szut/lf8_starter/employee/dto/EmployeeAddDto.java
+++ src/main/java/de/szut/lf8_starter/employee/dto/EmployeeAddDto.java
@@ -1,6 +1,5 @@
 package de.szut.lf8_starter.employee.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,9 +10,6 @@ public class EmployeeAddDto {
 
     @NotNull(message = "employee id must not be null")
     private Long employeeId;
-
-    @NotBlank(message = "employee role must not be blank")
-    private String qualification;
 
     @NotNull(message = "role id must not be null")
     private Long roleId;
